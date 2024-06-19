ed2a8106121f28845eb8992e09409238ae1e4207
Meceline <celinemercadier@gmail.com>
2024-06-19 11:21:57 +0200

    add swagger

--- back/src/main/java/com/openclassrooms/mddapi/configuration/SpringSecurityConfig.java
+++ back/src/main/java/com/openclassrooms/mddapi/configuration/SpringSecurityConfig.java
@@ -33,7 +33,11 @@ public class SpringSecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/register","/api/auth/login").permitAll()
+                        .requestMatchers("/api/auth/register",
+                                "/api/auth/login",
+                                "/swagger*/**",
+                                "/v3/api-docs",
+                                "/v3/api-docs/swagger-config").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
